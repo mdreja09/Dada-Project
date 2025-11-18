b0f596adece7b179c41f7b2ec07e508507123c0e
Reja <almialsama@gmail.com>
2025-11-18 20:56:03 +0600

    EditProfile

--- lib/EditProfile.dart
+++ lib/EditProfile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl_mobile_field/intl_mobile_field.dart';
 
 class Editprofile extends StatefulWidget {
   const Editprofile({super.key});
@@ -29,6 +30,72 @@ class _EditprofileState extends State<Editprofile> {
         ),
       ),
 
+
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: 16),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Profile Information",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 13),
+
+              Text("Name",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 18),),
+
+              //TextField use
+              SizedBox(height: 13),
+
+
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: "Your Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+
+              //Mobile Number Text
+              SizedBox(height: 13),
+
+              Text("Mobile Number",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),),
+
+              //Mobile Number Country
+              SizedBox(height: 13),
+
+              IntlMobileField(
+                initialCountryCode: 'BD', // initial Country is Bangladesh
+                languageCode: "en", // initial language is English
+                decoration: const InputDecoration(
+                  labelText: 'Mobile Number',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(),
+                  ),
+                ),
+                disableLengthCounter: true,
+              ),
+
+
+
+
+
+
+
+
+
+            ],
+          ),
+        ),
+
+      ),
 
     );
   }
