8d0892f4f607382760d1e18bdb17ae6511244c60
mdreja09 <muhammadreja0099@gmail.com>
2025-12-16 23:14:07 +0600

    Phone number validator

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -10,6 +10,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginscreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
   bool isBlue = true;
   @override
   Widget build(BuildContext context) {
@@ -48,7 +49,11 @@ class _LoginscreenState extends State<LoginScreen> {
                         ),
                       ),
                       SizedBox(height: 3, width: 179),
-                      Container(height: 3, width: 179, color: Color(0xffF4A758)),
+                      Container(
+                        height: 3,
+                        width: 179,
+                        color: Color(0xffF4A758),
+                      ),
                     ],
                   ),
                 ),
@@ -61,20 +66,28 @@ class _LoginscreenState extends State<LoginScreen> {
                       ///
                       /// Register Page Log IN
                       InkWell(
-                        onTap: (
-                            ) {
+                        onTap: () {
                           setState(() {
                             isBlue = !isBlue;
-                            Navigator.push(context,MaterialPageRoute(builder: (context)=>LoginScreen()));
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LoginScreen(),
+                              ),
+                            );
                           });
                         },
 
                         //// Register Now
-
                         child: InkWell(
                           focusColor: Colors.amber,
-                          onTap: (){
-                            Navigator.push(context,MaterialPageRoute(builder: (context)=>LoginRegister()));
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LoginRegister(),
+                              ),
+                            );
                           },
                           child: Text(
                             "Register Now",
@@ -98,215 +111,237 @@ class _LoginscreenState extends State<LoginScreen> {
             //2nd part in cloumn
             Padding(
               padding: const EdgeInsets.only(top: 17, left: 29),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 29, width: 178),
-                  Text(
-                    "Welcome back!",
-                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22),
-                  ),
-                  SizedBox(width: 18),
-                  Text(
-                    "Please enter your details to login",
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-                  ),
-
-                  SizedBox(width: 171, height: 21),
-
-                  Text(
-                    "Phone Number",
-                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
-                  ),
-
-                  SizedBox(height: 16, width: 171),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 16),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        focusColor: Colors.blueAccent,
-                        hintText: "Enter Your Phone Number",
-                      ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 29, width: 178),
+                    Text(
+                      "Welcome back!",
+                      style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22),
                     ),
-                  ),
-
-                  //3nd part
-                  SizedBox(width: 171, height: 21),
-
-                  Text(
-                    "Password",
-                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
-                  ),
-
-                  SizedBox(height: 17, width: 171),
-
-                  Padding(
-                    padding: const EdgeInsets.only(right: 16),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        suffixIcon: Icon(Icons.visibility_off),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-
-                        focusColor: Colors.blueAccent,
-                        hintText: "***********",
-                      ),
+                    SizedBox(width: 18),
+                    Text(
+                      "Please enter your details to login",
+                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 20,
+                
+                    SizedBox(width: 171, height: 21),
+                
+                    Text(
+                      "Phone Number",
+                      style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
                     ),
+                
+                    SizedBox(height: 16, width: 171),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 16),
+                      
+                      child: TextFormField(
+                        validator: (v){
+                          if( v== null || v.isEmpty){
+                            return "Please Enter Number";
+                          }if (!RegExp(r'^01[3-9]\d{8}$').hasMatch(v)) {
+                            return "Enter a valid Bangladeshi phone number";
+                          }
 
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Forgot Password",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                            color: Color(0xffF4A758),
+                          return null;
+                        },
+
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
                           ),
+                          focusColor: Colors.blueAccent,
+                          hintText: "Enter Your Phone Number",
+                        ),
+                      ),
+                    ),
+                
+                    //3nd part
+                    SizedBox(width: 171, height: 21),
+                
+                    Text(
+                      "Password",
+                      style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
+                    ),
+                
+                    SizedBox(height: 17, width: 171),
+                
+                    Padding(
+                      padding: const EdgeInsets.only(right: 16),
+                      child: TextFormField(
+
+
+                        decoration: InputDecoration(
+                          suffixIcon: Icon(Icons.visibility_off),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                
+                          focusColor: Colors.blueAccent,
+                          hintText: "***********",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 20,
+                      ),
+                
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Forgot Password",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xffF4A758),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    //////////////////////////////
+                    //////////////////////
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => Home()),
+                        );
+                      },
+                      child: Container(
+                        height: 52,
+                        width: 420,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(width: 1),
+                          color: Color(0xffF4A758),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "LogIn",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(width: 100, height: 60),
+                        Container(
+                          height: 2,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.grey),
+                        ),
+                        Text("OR", style: TextStyle(fontSize: 18)),
+                        Container(
+                          height: 2,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.grey),
                         ),
                       ],
                     ),
-                  ),
-                  //////////////////////////////
-                  //////////////////////
-                  GestureDetector(
-                    onTap: (){
-                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Home()));
-
-                    },
-                    child: Container(
+                
+                    //Google Sign in with google
+                    Container(
                       height: 52,
                       width: 420,
+                
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         border: Border.all(width: 1),
-                        color: Color(0xffF4A758),
+                
+                        color: Color(0xffF4A7580F).withOpacity(0.12),
                       ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/google.png", height: 30, width: 30),
+                
+                          SizedBox(width: 18),
+                
+                          Text(
+                            "Sig in with google",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                
+                      //child: Text("Sig in with google"),
+                    ),
+                
+                    ////...........Facebook Account
+                    SizedBox(height: 35),
+                    Container(
+                      height: 52,
+                      width: 420,
+                
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(width: 1),
+                
+                        color: Color(0xffF4A7580F).withOpacity(0.12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/facebook.png",
+                            height: 30,
+                            width: 30,
+                          ),
+                
+                          SizedBox(width: 18),
+                
+                          Text(
+                            "Sig in with Facebook",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                
+                      //child: Text("Sig in with google"),
+                    ),
+                    SizedBox(
+                      height: 67,
                       child: Center(
-                        child: Text(
-                          "LogIn",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(width: 100, height: 60),
-                      Container(
-                        height: 2,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.grey),
-                      ),
-                      Text("OR", style: TextStyle(fontSize: 18)),
-                      Container(
-                        height: 2,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-
-                  //Google Sign in with google
-                  Container(
-                    height: 52,
-                    width: 420,
-
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(width: 1),
-
-                      color: Color(0xffF4A7580F).withOpacity(0.12),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset("assets/google.png", height: 30, width: 30),
-
-                        SizedBox(width: 18),
-
-                        Text(
-                          "Sig in with google",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-
-                    //child: Text("Sig in with google"),
-                  ),
-
-                  ////...........Facebook Account
-                  SizedBox(height: 35),
-                  Container(
-                    height: 52,
-                    width: 420,
-
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(width: 1),
-
-                      color: Color(0xffF4A7580F).withOpacity(0.12),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset("assets/facebook.png", height: 30, width: 30),
-
-                        SizedBox(width: 18),
-
-                        Text(
-                          "Sig in with Facebook",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-
-                    //child: Text("Sig in with google"),
-                  ),
-                  SizedBox(
-                    height: 67,
-                    child: Center(
-                      child: RichText(
-                        text: TextSpan(
-                          text: "Already have an account?",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
-                            color: Color(0xff424242),
-                          ),
-                          children: [
-                            TextSpan(
-                              text: "Register here",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xffF4A758),
-                              ),
+                        child: RichText(
+                          text: TextSpan(
+                            text: "Already have an account?",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: Color(0xff424242),
                             ),
-                          ],
+                            children: [
+                              TextSpan(
+                                text: "Register here",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xffF4A758),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
